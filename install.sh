13b8695a13f17ab681e9c6ec854f3378d9aec56e
Dandri || ZNƏ <146328983+ZeltNamizake@users.noreply.github.com>
2024-02-03 20:10:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
  #!/system/bin/sh
- apt update && apt upgrade
+apt update && apt upgrade
 pkg install nodejs
-npm install axios clear-console figlet fs-extra https igdown-scrapper readline ytdl-core
-echo
+yarn
 echo "[¤] - Run script node zelt"
 echo
